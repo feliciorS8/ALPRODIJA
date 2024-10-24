de6adbdc972078b71f575f5efb7cd5d53be19590
feliciorS8 <onlydija33@gmail.com>
2024-10-24 03:16:30 -0800

    Add files via upload

---
+++ TUGAS ALPRO dija/total_harga.1.py
@@ -0,0 +1,7 @@
+harga = int(input("harga :"))
+jumlah = int(input("jumlah barang"))
+total = harga*jumlah
+print("total:", total)
+bayar = int(input("jumlah bayar:"))
+kembalian = bayar-total
+print("kembalian:",kembalian)
