29fa9e0528d77b9e061dab4e190624134ef621a7
feliciorS8 <onlydija33@gmail.com>
2025-03-09 23:04:09 -0800

    Add files via upload

---
+++ alpro pratikum/tipedata.py
@@ -0,0 +1,23 @@
+#string : 
+data1 = "belajar pyhton"
+#integer:
+data2 = 10
+#float:
+data3 = 3.14
+#list:
+data4 =["apel", "mangga", "jeruk"]
+#tuple
+data5 = ("apel", "mangga", "jeruk")
+#set =
+data6 = {"apel", "mangga", "jeruk"}
+#dictionary
+data7 = {"nama": "andi", "umur": 21}
+
+#tampilkan tipe data
+print("data1 tipe datanya : ",type(data1))
+print("data2 tipe datanya : ",type(data2))          
+print("data3 tipe datanya : ",type(data3))
+print("data4 tipe datanya : ",type(data4))
+print("data5 tipe datanya : ",type(data5))
+print("data6 tipe datanya : ",type(data6))
+print("data7 tipe datanya : ",type(data7))
